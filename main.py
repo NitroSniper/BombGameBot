a2ef889467b6f2245a5b556512f356ec1dcdfaa3
NitroSniper <NitroSniper@users.noreply.github.com>
2022-04-17 17:42:36 +0100

    Figuring out how to get javascript values.

--- main.py
+++ main.py
@@ -6,22 +6,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementNotVisibleException, ElementNotSelectableException
+import time
 
+def is_element_visible(element):
+    return element.value_of_css_property('display') != 'none'
 
 def main():
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    WAIT = WebDriverWait(driver, 10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
+    badwait = WebDriverWait(driver, 10)
+    # RoomCode = input("Enter Room Code: ")
+    RoomCode = 'NKNR'
+    driver.get(f"https://jklm.fun/{RoomCode}")
+    #info = WAIT.until(EC.presence_of_element_located((By.CLASS_NAME, 'styled nickname')))
+    driver.implicitly_wait(6)
+    test = driver.find_element(By.CLASS_NAME, 'styled.nickname')
+    test.clear()
+    test.send_keys('Nitro Sniper\n')
+    driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+    #print (driver.page_source)
+    joined = driver.find_element(By.CLASS_NAME, 'joined')
+    seating = driver.find_element(By.CLASS_NAME, 'seating')
+    
+    while True:
+        if not seating.is_displayed() and is_element_visible(joined): # if the game has started
+            print ("Joined and Seating")
+            print (driver.find_element(By.CLASS_NAME, 'syllable').text)
+            # print(driver.execute_script('return milestone.playerStatesByPeerId[milestone.currentPlayerPeerId]'))
+            print (driver.execute_script('return JSON.stringify(players[0], null, 4)'))
 
-    driver.get(f"https://techwithtim.net/")
-    print (driver.title)
+        time.sleep(1)
     input("Press enter to continue...")
-    #search = driver.find_element_by_name('s')
-    search = driver.find_element(By.NAME, 's')
-    search.send_keys('test')
-    search.send_keys(Keys.RETURN)
-    input("Press enter to continue...")
-    driver.quit()
-
 if __name__ == "__main__":
     main()
